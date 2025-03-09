1d4b8de3a8908f41485f402f32c45f01455a9129
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-03-09 21:52:33 +0100

    webapi tests for category

---
+++ tests/Eshop.WebApi.Tests/Features/Categories/GetCategoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Eshop.Domain;
+using Eshop.WebApi.Exceptions;
+using Eshop.WebApi.Features.Categories;
+using Snapper;
+
+namespace Eshop.WebApi.Tests.Features.Categories
+{
+    public class GetCategoryTests : TestBase
+    {
+        [SetUp]
+        public async Task Seed()
+        {
+            var category = await dbContext.Categories.AddAsync(new Category(0, "Category 1"));
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+        }
+
+        [Test]
+        public async Task GetCategory_ReturnsCorrectDto()
+        {
+            // arrange
+            var query = new GetCategory.Query(1);
+            var handler = new GetCategory.Handler(dbContext);
+
+            // act
+            var sut = await handler.Handle(query, CancellationToken.None);
+
+            // assert
+            sut.ShouldMatchSnapshot();
+        }
+
+        [Test]
+        public void GetCategory_ThrowsArgumentNotFoundException()
+        {
+            // act
+            var query = new GetCategory.Query(2);
+            var handler = new GetCategory.Handler(dbContext);
+
+            // assert
+            Assert.ThrowsAsync<NotFoundException>(() => handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Eshop.WebApi.Tests/Features/Categories/ValidatorTests/AddCategoryValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Eshop.WebApi.Features.Categories;
+using Eshop.WebApi.Features.Products;
+using FluentValidation.TestHelper;
+
+namespace Eshop.WebApi.Tests.Features.Categories.ValidatorTests
+{
+    public class AddCategoryValidatorTests
+    {
+        private AddCategory.Validator? validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new AddCategory.Validator();
+        }
+
+        [Test]
+        public void AddCategory_Validator_Name_Is_Not_Empty()
+        {
+            var requestDto = new AddCategoryRequestDto { Name = " " };
+            var command = new AddCategory.Command(requestDto);
+            var result = validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(x => x.Request.Name);
+        }
+    }
+}
