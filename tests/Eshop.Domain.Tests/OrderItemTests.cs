329f76eb09c287ed4f679034676218f710e0e30e
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-03-21 22:07:03 +0100

    added Order and OrderItem for domain, EF and domain tests

---
+++ tests/Eshop.Domain.Tests/OrderItemTests.cs
@@ -0,0 +1,92 @@
+﻿namespace Eshop.Domain.Tests
+{
+    public class OrderItemTests
+    {
+        [Test]
+        public void OrderItem_WithValidParams_SetCorrectlyProperties()
+        {
+            // arrange
+            var id = 0;
+            var productId = 1;
+            var quantity = 2;
+            var price = 100;
+            var orderId = 1;
+            var order = new Order(0, DateTime.Now.Ticks, "Notebooks", 200, "New"); // Example Order setup
+
+            // act
+            var sut = new OrderItem(id, productId, quantity, price, orderId, order);
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(sut.Id, Is.EqualTo(id));
+                Assert.That(sut.ProductId, Is.EqualTo(productId));
+                Assert.That(sut.Quantity, Is.EqualTo(quantity));
+                Assert.That(sut.Price, Is.EqualTo(price));
+                Assert.That(sut.OrderId, Is.EqualTo(orderId));
+            });
+        }
+
+        [Test]
+        public void OrderItem_WithInvalidIdParam_ThrowsException()
+        {
+            // arrange
+            var productId = 1;
+            var quantity = 2;
+            var price = 100;
+            var orderId = 1;
+            var order = new Order(0, DateTime.Now.Ticks, "Notebooks", 200, "New");
+
+            // act/assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new OrderItem(-1, productId, quantity, price, orderId, order));
+        }
+
+        [TestCase(0, 2, 100, 1)]
+        [TestCase(1, -1, 100, 1)]
+        [TestCase(1, 2, -100, 1)]
+        public void OrderItem_WithInvalidParams_ThrowsException(int productId, int quantity, decimal price, int orderId)
+        {
+            // arrange
+            var order = new Order(0, DateTime.Now.Ticks, "Notebooks", 200, "New");
+
+            // act/assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(0, productId, quantity, price, orderId, order));
+        }
+
+        [Test]
+        public void OrderItemUpdate_WithValidParams_SetCorrectlyProperties()
+        {
+            // arrange
+            var newProductId = 2;
+            var newQuantity = 3;
+            var newPrice = 150;
+            var orderId = 1;
+            var order = new Order(0, DateTime.Now.Ticks, "Notebooks", 200, "New");
+            var sut = new OrderItem(0, 1, 2, 100, orderId, order);
+
+            // act
+            sut.Update(newProductId, newQuantity, newPrice);
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(sut.ProductId, Is.EqualTo(newProductId));
+                Assert.That(sut.Quantity, Is.EqualTo(newQuantity));
+                Assert.That(sut.Price, Is.EqualTo(newPrice));
+            });
+        }
+
+        [TestCase(0, 2, 100)]
+        [TestCase(1, -1, 100)]
+        [TestCase(1, 2, -100)]
+        public void OrderItemUpdate_WithInvalidParams_ThrowsException(int productId, int quantity, decimal price)
+        {
+            // arrange
+            var order = new Order(0, DateTime.Now.Ticks, "Notebooks", 200, "New");
+            var sut = new OrderItem(0, 1, 2, 100, 1, order);
+
+            // act/assert
+            Assert.Throws<ArgumentException>(() => sut.Update(productId, quantity, price));
+        }
+    }
+}
---
+++ tests/Eshop.Domain.Tests/OrderTests.cs
@@ -0,0 +1,86 @@
+﻿namespace Eshop.Domain.Tests
+{
+    public class OrderTests
+    {
+        [Test]
+        public void Order_WithValidParams_SetCorrectlyProperties()
+        {
+            // arrange
+            var id = 0;
+            var orderDate = DateTime.Now.Ticks;
+            var orderItems = "Order Items";
+            var totalPrice = 100;
+            var status = "Active";
+
+            // act
+            var sut = new Order(id, orderDate, orderItems, totalPrice, status);
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(sut.Id, Is.EqualTo(id));
+                Assert.That(sut.OrderDate, Is.EqualTo(orderDate));
+                Assert.That(sut.OrderItems, Is.EqualTo(orderItems));
+                Assert.That(sut.TotalPrice, Is.EqualTo(totalPrice));
+                Assert.That(sut.Status, Is.EqualTo(status));
+            });
+        }
+
+        [Test]
+        public void Order_WithInvalidIdParam_ThrowsException()
+        {
+            // act/assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Order(-1, DateTime.Now.Ticks, "Order Items", 100, "Active"));
+        }
+
+        [TestCase(0, " ", 100, "Active", typeof(ArgumentNullException))]
+        [TestCase(0, "Order Items", -1, "Active", typeof(ArgumentOutOfRangeException))]
+        [TestCase(0, null, 100, "Active", typeof(ArgumentNullException))]
+        [TestCase(0, "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 100, "Active", typeof(ArgumentOutOfRangeException))]
+        [TestCase(-1, "Order Items", 100, "Active", typeof(ArgumentOutOfRangeException))]
+        public void Order_WithInvalidParams_ThrowsException(long orderDate, string? orderItems, int totalPrice, string? status, Type expectedException)
+        {
+            // act/assert
+            Assert.Throws(expectedException, () => new Order(0, orderDate, orderItems!, totalPrice, status!));
+        }
+
+
+        [Test]
+        public void OrderUpdate_WithValidParams_SetCorrectlyProperties()
+        {
+            // arrange
+            var newOrderDate = DateTime.Now.Ticks;
+            var newOrderItems = "Updated Order Items";
+            var newTotalPrice = 200;
+            var newStatus = "Completed";
+            var sut = new Order(0, DateTime.Now.Ticks, "Order Items", 100, "Active");
+
+            // act
+            sut.Update(newOrderDate, newOrderItems, newTotalPrice, newStatus);
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(sut.OrderDate, Is.EqualTo(newOrderDate));
+                Assert.That(sut.OrderItems, Is.EqualTo(newOrderItems));
+                Assert.That(sut.TotalPrice, Is.EqualTo(newTotalPrice));
+                Assert.That(sut.Status, Is.EqualTo(newStatus));
+            });
+        }
+
+        [TestCase(-1, "Updated Order Items", 200, "Completed", typeof(ArgumentOutOfRangeException))]
+        [TestCase(0, null, 200, "Completed", typeof(ArgumentNullException))]
+        [TestCase(0, " ", 200, "Completed", typeof(ArgumentNullException))]
+        [TestCase(0, "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 200, "Completed", typeof(ArgumentOutOfRangeException))]
+        [TestCase(0, "Updated Order Items", -1, "Completed", typeof(ArgumentOutOfRangeException))]
+        public void OrderUpdate_WithInvalidParams_ThrowsException(long orderDate, string? orderItems, int totalPrice, string? status, Type expectedException)
+        {
+            // arrange
+            var sut = new Order(0, DateTime.Now.Ticks, "Order Items", 100, "Active");
+
+            // act/assert
+            Assert.Throws(expectedException, () => sut.Update(orderDate, orderItems!, totalPrice, status!));
+        }
+
+    }
+}
