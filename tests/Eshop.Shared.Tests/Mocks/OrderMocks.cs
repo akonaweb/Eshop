c8002099799a75e8a069ce8a48602a586746e125
akonaweb <akonaweb.sk@gmail.com>
2025-04-09 21:25:50 +0200

    Eshop.Shared.Tests project - mocks for Domain & WebApi tests

---
+++ tests/Eshop.Shared.Tests/Mocks/OrderMocks.cs
@@ -0,0 +1,9 @@
+﻿using Eshop.Domain;
+
+namespace Eshop.Shared.Tests.Mocks
+{
+    public static class OrderMocks
+    {
+        public static Order GetOrder1() => new(1, "Customer 1", "Address 1");
+    }
+}
