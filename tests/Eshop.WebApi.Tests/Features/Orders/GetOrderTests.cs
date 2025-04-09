40fbc3c5091f50215f2ba955692235a4d4dac9cd
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-04-09 20:51:40 +0200

    Added tests for get endpoint in order and order item

---
+++ tests/Eshop.WebApi.Tests/Features/Orders/GetOrderTests.cs
@@ -0,0 +1,42 @@
+﻿using Eshop.Domain;
+using Eshop.WebApi.Exceptions;
+using Eshop.WebApi.Features.Orders;
+using Snapper;
+
+namespace Eshop.WebApi.Tests.Features.Orders
+{
+    public class GetOrderTests : TestBase
+    {
+        [Test]
+        public async Task GetOrder_ReturnsCorrectDto()
+        {
+            // arrange
+            var product = new Product(0, "Product 1", "Description", 1, null);
+            await dbContext.Products.AddAsync(product, CancellationToken.None);
+            var order = new Order(0, "Customer", "Address");
+            var orderItem = new OrderItem(1, 1, product);
+            order.AddItem(orderItem);
+            await dbContext.Orders.AddAsync(order, CancellationToken.None);
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+            var query = new GetOrder.Query(order.Id);
+            var handler = new GetOrder.Handler(dbContext);
+
+            // act
+            var sut = await handler.Handle(query, CancellationToken.None);
+
+            // assert
+            sut.ShouldMatchSnapshot();
+        }
+
+        [Test]
+        public void GetOrder_WithInvalidId_ThrowsNotFoundException()
+        {
+            // arrange
+            var query = new GetOrder.Query(1);
+            var handler = new GetOrder.Handler(dbContext);
+
+            // act/assert
+            Assert.ThrowsAsync<NotFoundException>(async () => await handler.Handle(query, CancellationToken.None));
+        }
+    }
+}
---
+++ tests/Eshop.WebApi.Tests/Features/Orders/ValidatorTests/GetOrderValidationTests.cs
@@ -0,0 +1,32 @@
+﻿using Eshop.WebApi.Features.Orders;
+using FluentValidation.TestHelper;
+
+namespace Eshop.WebApi.Tests.Features.Orders.ValidatorTests
+{
+    public class GetOrderValidationTests
+    {
+        private GetOrder.Validator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new GetOrder.Validator();
+        }
+
+        [Test]
+        public void GetOrderValidator_IdMustBeGreaterThanZero()
+        {
+            var command = new GetOrder.Query(0);
+            var result = validator.TestValidate(command);
+            result.ShouldHaveValidationErrorFor(x => x.Id);
+        }
+
+        [Test]
+        public void GetOrderValidator_IsValid()
+        {
+            var command = new GetOrder.Query(1);
+            var result = validator.TestValidate(command);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
