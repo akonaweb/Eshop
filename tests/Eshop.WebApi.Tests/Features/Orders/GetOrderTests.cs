74f09e1dc52eac32e56d5228a82480ba09360c08
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-04-10 20:56:37 +0200

    small fixes

--- tests/Eshop.WebApi.Tests/Features/Orders/GetOrderTests.cs
+++ tests/Eshop.WebApi.Tests/Features/Orders/GetOrderTests.cs
@@ -2,7 +2,6 @@
 using Eshop.WebApi.Features.Orders;
 using Eshop.Domain;
 using Snapper;
-using Microsoft.EntityFrameworkCore;
 
 namespace Eshop.WebApi.Tests.Features.Orders
 {
@@ -14,7 +13,7 @@ namespace Eshop.WebApi.Tests.Features.Orders
             // arrange
             var order = new Order(0, "Customer", "Address");
             order.AddItem(new OrderItem(1, 1, new Product(0, "Product 1", "Description", 1, null)));
-            await dbContext.Orders.AnyAsync(CancellationToken.None);
+            await dbContext.Orders.AddAsync(order, CancellationToken.None);
             await dbContext.SaveChangesAsync(CancellationToken.None);
             var query = new GetOrder.Query(order.Id);
             var handler = new GetOrder.Handler(dbContext);
