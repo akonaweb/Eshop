65acb5c49d555a678946284482a8adaee7f6403f
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-07-26 15:23:00 +0200

    Refactored Change Pasword Endpint

---
+++ tests/Eshop.WebApi.Tests/Features/Users/ForgotPasswordTests.cs
@@ -0,0 +1,55 @@
+﻿using Eshop.Persistence;
+using Eshop.WebApi.Features.Users;
+using Microsoft.AspNetCore.Identity.Data;
+using Moq;
+using Snapper;
+
+namespace Eshop.WebApi.Tests.Features.Users
+{
+    public class ForgotPasswordTests : TestBase
+    {
+        [Test]
+        public async Task ForgotPassword_WithCorrectCredentials_ReturnTokens()
+        {
+            // arrange
+            var email = "test@test.com";
+            var user = new ApplicationUser(email);
+            var token = "test-reset-token";
+
+            userManagerMock.Setup(um => um.FindByEmailAsync(email)).ReturnsAsync(user);
+            userManagerMock.Setup(um => um.GeneratePasswordResetTokenAsync(user)).ReturnsAsync(token);
+
+            var request = new ForgotPasswordRequest { Email = email };
+            var command = new ForgotPassword.Command(request);
+            var handler = new ForgotPassword.Handler(userManagerMock.Object);
+
+            // act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // assert
+            result.ShouldMatchSnapshot();
+        }
+
+        [Test]
+        public async Task ForgotPassword_WhenUserNotFound_ReturnsNotFound()
+        {
+            // arrange
+            var email = "test@test.com";
+            ApplicationUser user = null!;
+            var token = "test-reset-token";
+
+            userManagerMock.Setup(um => um.FindByEmailAsync(email)).ReturnsAsync(user);
+            userManagerMock.Setup(um => um.GeneratePasswordResetTokenAsync(user)).ReturnsAsync(token);
+
+            var request = new ForgotPasswordRequest { Email = email };
+            var command = new ForgotPassword.Command(request);
+            var handler = new ForgotPassword.Handler(userManagerMock.Object);
+
+            // act
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // assert
+            result.ShouldMatchSnapshot();
+        }
+    }
+}
---
+++ tests/Eshop.WebApi.Tests/Features/Users/ValidatorTests/ForgotPasswordValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Eshop.WebApi.Features.Users;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace Eshop.WebApi.Tests.Features.Users.ValidatorTests
+{
+    public class ForgotPasswordValidatorTests
+    {
+        private ForgotPassword.Validator validator = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new ForgotPassword.Validator();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void ForgotPassword_EmailNotNullOrEmpty(string? email)
+        {
+            var request = new ForgotPasswordRequest {  Email = email! };
+            var command = new ForgotPassword.Command(request);
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(x => x.Request.Email);
+        }
+    }
+}
