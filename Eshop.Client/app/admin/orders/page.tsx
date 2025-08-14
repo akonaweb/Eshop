f8276427e02e05f780f521dc2117fcb45850c7f1
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2025-08-14 09:48:45 +0200

    Updated Order Table

--- Eshop.Client/app/admin/orders/page.tsx
+++ Eshop.Client/app/admin/orders/page.tsx
@@ -1,4 +1,12 @@
 import { cookies } from "next/headers";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from "next/link";
 
 import { getOrders } from "@/api/orders";
 
@@ -10,14 +18,27 @@ export default async function AdminOrdersPage() {
   return (
     <>
       <h1>Management Orders</h1>
-
-      <ul>
-        {orders.map((x) => (
-          <li key={x.id}>
-            {x.id} - {x.customer} - {x.address}
-          </li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Order ID</TableCell>
+              <TableCell>Customer</TableCell>
+              <TableCell>Address</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell><Link href={`/admin/orders/${order.id}`}>{order.id}</Link></TableCell>
+                <TableCell>{order.customer}</TableCell>
+                <TableCell>{order.address}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <p>Total Orders: {orders.length}</p>
     </>
   );
 }
